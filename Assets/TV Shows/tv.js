2fc7d6160a0f87498eb04d8b09c94603da63b698
JacobFullerOBU <Fuller.Jacob@unionps.org>
2026-01-05 15:18:20 -0600

    add shows to tv.js

--- Assets/TV Shows/tv.js
+++ Assets/TV Shows/tv.js
@@ -28,5 +28,292 @@ export const tv = [
         year: 2016,
         reviews: 134,
         image: "cardimage.jpeg"
+    },
+    {
+        id: 7,
+        title: "The Sopranos",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/6/6e/Sopranos_titlescreen.png/250px-Sopranos_titlescreen.png"
+    },
+    {
+        id: 8,
+        title: "The Wire",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/The_Wire_logo.svg/250px-The_Wire_logo.svg.png"
+    },
+    {
+        id: 9,
+        title: "Mad Men",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/en/3/33/Mad-men-title-card.jpg"
+    },
+    {
+        id: 10,
+        title: "Friends",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Friends_logo.svg/250px-Friends_logo.svg.png"
+    },
+    {
+        id: 11,
+        title: "The Simpsons",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/The_Simpsons_yellow_logo.svg/250px-The_Simpsons_yellow_logo.svg.png"
+    },
+    {
+        id: 12,
+        title: "Seinfeld",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Seinfeld_logo.svg/250px-Seinfeld_logo.svg.png"
+    },
+    {
+        id: 13,
+        title: "Office",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Office_Tamil_serial.jpg/250px-Office_Tamil_serial.jpg"
+    },
+    {
+        id: 14,
+        title: "The Twilight Zone",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Thetwilightzone-logo.svg/250px-Thetwilightzone-logo.svg.png"
+    },
+    {
+        id: 15,
+        title: "Succession",
+        description: "",
+        category: "tv",
+        image: ""
+    },
+    {
+        id: 16,
+        title: "Better Call Saul",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/8/8a/Better_Call_Saul_logo.svg/250px-Better_Call_Saul_logo.svg.png"
+    },
+    {
+        id: 17,
+        title: "The Crown",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/1/18/The_Crown_Title_Card.jpg/250px-The_Crown_Title_Card.jpg"
+    },
+    {
+        id: 18,
+        title: "The West Wing",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/The_West_Wing_%28NBC_TV_series%29_logo.svg/250px-The_West_Wing_%28NBC_TV_series%29_logo.svg.png"
+    },
+    {
+        id: 19,
+        title: "Fargo",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Fargo_%28TV_logo%29.svg/250px-Fargo_%28TV_logo%29.svg.png"
+    },
+    {
+        id: 20,
+        title: "Curb Your Enthusiasm",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Curb_Your_Enthusiasm.png/250px-Curb_Your_Enthusiasm.png"
+    },
+    {
+        id: 21,
+        title: "The Mandalorian",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/The_Mandalorian.svg/250px-The_Mandalorian.svg.png"
+    },
+    {
+        id: 22,
+        title: "House of the Dragon",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/House_of_the_dragon_logo.png/250px-House_of_the_dragon_logo.png"
+    },
+    {
+        id: 23,
+        title: "Twin Peaks",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Twin_Peaks_title.svg/250px-Twin_Peaks_title.svg.png"
+    },
+    {
+        id: 24,
+        title: "The X-Files",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/5/5e/The_X-Files_original_title_screen.webp/250px-The_X-Files_original_title_screen.webp.png"
+    },
+    {
+        id: 25,
+        title: "Arrested Development",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Arrested_Development.svg/250px-Arrested_Development.svg.png"
+    },
+    {
+        id: 26,
+        title: "The Americans",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/The_americans_2013_title_logo.svg/250px-The_americans_2013_title_logo.svg.png"
+    },
+    {
+        id: 27,
+        title: "True Detective",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/en/5/5a/True_Detective_2014_Intertitle.jpg"
+    },
+    {
+        id: 28,
+        title: "Parks and Recreation",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Parks_and_Recreation_%28Universal_Television%29.svg/250px-Parks_and_Recreation_%28Universal_Television%29.svg.png"
+    },
+    {
+        id: 29,
+        title: "BoJack Horseman",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/BoJack_Horseman_Logo.svg/250px-BoJack_Horseman_Logo.svg.png"
+    },
+    {
+        id: 30,
+        title: "The Last of Us",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/The_Last_of_Us_logo.svg/250px-The_Last_of_Us_logo.svg.png"
+    },
+    {
+        id: 31,
+        title: "Ozark",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ozark_TV_series_logo.svg/250px-Ozark_TV_series_logo.svg.png"
+    },
+    {
+        id: 32,
+        title: "House",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/House_logo.svg/250px-House_logo.svg.png"
+    },
+    {
+        id: 33,
+        title: "Black Mirror",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Black_Mirror_%28British_TV_series%29_logo.svg/250px-Black_Mirror_%28British_TV_series%29_logo.svg.png"
+    },
+    {
+        id: 34,
+        title: "The Boys",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/The_Boys_TV_series_logo.svg/250px-The_Boys_TV_series_logo.svg.png"
+    },
+    {
+        id: 35,
+        title: "Sherlock",
+        description: "This is an accepted version of this page",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/4/4d/Sherlock_titlecard.jpg/250px-Sherlock_titlecard.jpg"
+    },
+    {
+        id: 36,
+        title: "Saturday Night Live",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Saturday_Night_Live_2024.svg/250px-Saturday_Night_Live_2024.svg.png"
+    },
+    {
+        id: 37,
+        title: "Downton Abbey",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c4/Downton_Abbey_Title_Card.jpg/250px-Downton_Abbey_Title_Card.jpg"
+    },
+    {
+        id: 38,
+        title: "Freaks and Geeks",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/en/0/06/Freaks_and_Geeks.jpg"
+    },
+    {
+        id: 39,
+        title: "Veep",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Veep_Logo.svg/250px-Veep_Logo.svg.png"
+    },
+    {
+        id: 40,
+        title: "Nathan for You",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/d/db/Nathan_For_You_title.png/250px-Nathan_For_You_title.png"
+    },
+    {
+        id: 41,
+        title: "The Leftovers",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/3/39/LeftoversS2Logo.jpg/250px-LeftoversS2Logo.jpg"
+    },
+    {
+        id: 42,
+        title: "The Good Place",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/The_Good_Place_title_card.svg/250px-The_Good_Place_title_card.svg.png"
+    },
+    {
+        id: 43,
+        title: "The Haunting of Hill House",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/0/04/HauntingOfHillHouse.JPG/250px-HauntingOfHillHouse.JPG"
+    },
+    {
+        id: 44,
+        title: "Harry Potter",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/2/29/Harry_Potter_%28TV_series%29_logo.png/250px-Harry_Potter_%28TV_series%29_logo.png"
+    },
+    {
+        id: 45,
+        title: "The Big Bang Theory",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/7/7b/The_Big_Bang_Theory_%28Official_Title_Card%29.png/250px-The_Big_Bang_Theory_%28Official_Title_Card%29.png"
+    },
+    {
+        id: 46,
+        title: "24",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/24-Logo.svg/250px-24-Logo.svg.png"
+    },
+    {
+        id: 47,
+        title: "How I Met Your Mother",
+        description: "",
+        category: "tv",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/HowIMetYourMother.svg/250px-HowIMetYourMother.svg.png"
     }
 ];
